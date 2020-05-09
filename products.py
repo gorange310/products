cc0507b86b1fd8440804371374f35340b3f708b8
Orange <orange@Orangede-MacBook-Pro.local>
2020-05-09 16:40:17 +0800

    add check file

--- products.py
+++ products.py
@@ -1,20 +1,38 @@
-products =[]
+import os
+# check file
+if os.path.isfile('products.csv'):
+	print("There's your file!")
+else:
+	print("No file exists.")
 
+# read fil 
+products =[]
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if 'product, price' in line:
+			continue
+		name, price = line.strip().split(',')
+		
+
+# input file
 while True:
 	name = input('Enter the name of product: ')
 	if name == 'q':
 		break
 	price = input('Enter the price: ')
+	price = int(price)
 	products.append([name, price])
 	# p = []
 	# p.append(name)
 	# p.append(price)
 	# products.append(p)
 
-#print each product price
+#print each product
 for p in products:
 	print('The price of', p[0], 'is $', p[1])
 
-with open('products.csv', 'w') as f:
+# write file
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('Product,Price\n')
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+		f.write(p[0] + ',' + str(p[1]) + '\n')
