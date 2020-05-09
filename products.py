13218e4a3147386e84e0881bb33c7e2e97ec16bb
Orange <orange@Orangede-MacBook-Pro.local>
2020-05-09 18:42:39 +0800

    refactor

--- products.py
+++ products.py
@@ -1,32 +1,49 @@
-import os
-# check file and read file
-products =[]
-if os.path.isfile('products.csv'):
-	print("There's your file!")
-	with open('products.csv', 'r', encoding = 'utf-8') as f:
-		for line in f:
-			if 'Product,Price' in line:
-				continue
-			name, price = line.strip().split(',')
-			products.append([name, price])
-else:
-	print("No file exists.")
-		
+import os #operating system
+# read file
+def read_file(filename):
+    products =[]
+    with open(filename, 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if 'Product,Price' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    return products
+        
 # user inputs file
-while True:
-	name = input('Enter the name of product: ')
-	if name == 'q':
-		break
-	price = input('Enter the price: ')
-	price = int(price)
-	products.append([name,price])
+def user_input(products):
+    while True:
+        name = input('Enter the name of product: ')
+        if name == 'q':
+            break
+        price = input('Enter the price: ')
+        price = int(price)
+        products.append([name,price])
+    print(products)
+    return products
 
 # print each product
-for p in products:
-	print('The price of', p[0], 'is $', p[1])
+def print_products(products):
+    for p in products:
+        print(p[0], 'is $', p[1])
 
 # write file
-with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('Product,Price\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+def write_file(filename, products):
+    with open(filename, 'w', encoding = 'utf-8') as f:
+        f.write('Product,Price\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+    filename = 'products.csv'
+    if os.path.isfile(filename): # check file
+        print("There's your file!")
+        products = read_file(filename)
+    else:
+        print("No file exists.")
+
+    products = user_input(products)
+    print_products(products)
+    write_file('products.csv', products)
+
+main()
